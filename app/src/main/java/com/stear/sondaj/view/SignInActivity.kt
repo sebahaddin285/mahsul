4d5dc13f4abfc883d101e7bac695c6dde51919d2
stear <sebahaddin.mar@gmail.com>
2023-02-18 22:22:23 +0300

    registration screen designed

--- app/src/main/java/com/stear/sondaj/view/SignInActivity.kt
+++ app/src/main/java/com/stear/sondaj/view/SignInActivity.kt
@@ -9,11 +9,27 @@ import com.stear.sondaj.R
 import com.stear.sondaj.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var _binding : ActivitySignInBinding
+    private lateinit var _binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      _binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+        var intent: Intent? = null
 
+        _binding.signInButton.setOnClickListener() {
+            intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        _binding.signUpButton.setOnClickListener() {
+            intent = Intent(applicationContext,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        _binding.googleButton.setOnClickListener() {
+            intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
